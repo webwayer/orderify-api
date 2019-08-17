e5cc1cf398e6df0bd7abd85cfeea8a94e01bec8e
Rodion Balandin <webwayer@gmail.com>
2019-08-18 01:25:41 +0300

    session in postgres

--- src/routes/stateful/index.ts
+++ src/routes/stateful/index.ts
@@ -1,12 +1,30 @@
 import { Router } from 'express'
 import * as session from 'express-session'
+import { Sequelize} from 'sequelize'
 
 import { router as authRouter } from './auth'
 
 export const router = Router();
 
+const sequelize = new Sequelize(
+    "orderify",
+    "",
+    "", {
+        "dialect": "postgres",
+        "host": "localhost",
+        "port": 5432
+    }
+);
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelizeStore = new SequelizeStore({
+    db: sequelize
+})
+// sequelize.sync();
+
 router.use(session({
     secret: 'keyboard cat',
+    store: sequelizeStore,
     resave: false,
     saveUninitialized: true,
     // cookie: { secure: true }
