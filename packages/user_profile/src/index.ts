99b401bb3ad8e444376e04122af13cb0b813f6ab
Rodion Balandin <webwayer@gmail.com>
2019-09-30 02:54:19 +0300

    fix

--- packages/user_profile/src/index.ts
+++ packages/user_profile/src/index.ts
@@ -1,6 +1,6 @@
 export * from './lib/User'
 export * from './lib/AccessToken'
-export * from './lib/read.graphql'
+export * from './lib/user.query.graphql'
 export * from './lib/JWT'
 export * from './lib/authGuardRouter'
 export * from './lib/Auth'
