94adb76f4f0053b016adfce9fcfbaf6dddafbac1
Rodion Balandin <webwayer@gmail.com>
2019-09-29 23:50:08 +0300

    little bit updates

---
+++ packages/user_profile/src/lib/authGuardRouter.ts
@@ -0,0 +1,45 @@
+import { Router } from 'express'
+import { IAccessTokenStatic } from './AccessToken'
+import { verifyToken } from './jwt'
+
+export function authGuardRouterFactory(
+    AccessToken: IAccessTokenStatic,
+) {
+    const router = Router()
+
+    router.get('/favicon.ico', (req, res) => {
+        res.status(404).end()
+    })
+    // some graphqli thing i think
+    router.get('/unfetch.umd.js.map', (req, res) => {
+        res.status(404).end()
+    })
+
+    router.use(async (req, res, next) => {
+        try {
+            if (req.query.token) {
+                // if (req.headers.authorization) {
+                // const authHeader = req.headers.authorization.split('_')
+                // const tokenRaw = authHeader[1]
+                const tokenRaw = req.query.token
+                const token = verifyToken(tokenRaw)
+
+                const accessToken = AccessToken.findByPk(token.id)
+                if (accessToken) {
+                    // tslint:disable-next-line: no-string-literal
+                    req['userId'] = token.uid
+
+                    next()
+                } else {
+                    throw new Error('accessToken doesnt exist in database')
+                }
+            } else {
+                throw new Error('no accessToken provided')
+            }
+        } catch (err) {
+            next(err)
+        }
+    })
+
+    return router
+}
